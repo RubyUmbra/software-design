1b8869e3797cf799c739b2e8ceaa81c023155f2d
RubyUmbra <rubyumbra@gmail.com>
2020-10-27 06:59:28 +0300

    Refactor duplicate code

--- java/refactoring/src/main/java/ru/akirakozov/sd/refactoring/request/QueryRequest.java
+++ java/refactoring/src/main/java/ru/akirakozov/sd/refactoring/request/QueryRequest.java
@@ -6,4 +6,17 @@ public abstract class QueryRequest extends Request {
     QueryRequest(PrintWriter w) {
         super(w);
     }
+
+    public static QueryRequest getFromCommand(PrintWriter w, String command) {
+        if ("max".equals(command))
+            return new MaxRequest(w);
+        else if ("min".equals(command))
+            return new MinRequest(w);
+        else if ("sum".equals(command))
+            return new SumRequest(w);
+        else if ("count".equals(command))
+            return new CountRequest(w);
+        else
+            return new BadRequest(w, command);
+    }
 }
--- java/refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/QueryServlet.java
+++ java/refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/QueryServlet.java
@@ -16,78 +16,21 @@ public class QueryServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String command = request.getParameter("command");
 
-        Request r;
-        if ("max".equals(command)) {
-            r = new MaxRequest(response.getWriter());
-            try {
-                try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-                    Statement stmt = c.createStatement();
-                    ResultSet rs = stmt.executeQuery(r.sqlQuery);
-
-                    r.printResponse(rs);
-
-                    rs.close();
-                    stmt.close();
-                }
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        } else if ("min".equals(command)) {
-            r = new MinRequest(response.getWriter());
-            try {
-                try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-                    Statement stmt = c.createStatement();
-                    ResultSet rs = stmt.executeQuery(r.sqlQuery);
-
-                    r.printResponse(rs);
-
-                    rs.close();
-                    stmt.close();
-                }
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        } else if ("sum".equals(command)) {
-            r = new SumRequest(response.getWriter());
-            try {
-                try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-                    Statement stmt = c.createStatement();
-                    ResultSet rs = stmt.executeQuery(r.sqlQuery);
-
-                    r.printResponse(rs);
-
-                    rs.close();
-                    stmt.close();
-                }
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        } else if ("count".equals(command)) {
-            r = new CountRequest(response.getWriter());
-            try {
-                try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-                    Statement stmt = c.createStatement();
-                    ResultSet rs = stmt.executeQuery(r.sqlQuery);
-
-                    r.printResponse(rs);
-
-                    rs.close();
-                    stmt.close();
-                }
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        } else {
-            try {
-                r = new BadRequest(response.getWriter(), command);
+        Request r = QueryRequest.getFromCommand(response.getWriter(), command);
+        try {
+            if (r instanceof BadRequest) {
                 r.printResponse(null);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
+            } else {
+                try (
+                        Connection c = DriverManager.getConnection("jdbc:sqlite:test.db");
+                        Statement stmt = c.createStatement();
+                        ResultSet rs = stmt.executeQuery(r.sqlQuery)
+                ) {
+                    r.printResponse(rs);
+                }
             }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
 
         response.setContentType("text/html");
